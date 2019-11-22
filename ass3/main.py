9b5cbe8199ecdf31844b6dd3bfcc4a87c38015a8
HectorMtzm <31526833+HectorMtzm@users.noreply.github.com>
2019-11-22 17:41:25 -0600

    finished Assignment 3

--- ass3/main.py
+++ ass3/main.py
@@ -4,36 +4,51 @@ from __future__ import division       #                           ''
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 
-main():
+def main():
     #Initialize components
     BP = brickpi3.BrickPi3()
-    BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
+    BP.reset_all()
 
-    speedAdvanceToGetOverTheRailsOfTheTrain = 60
+    BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
+    BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
+    BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D))
+
+    speedAdvanceToGetOverTheRailsOfTheTrain = -60
     speedStop = 0
+
+    time.sleep(10)
+
+    def getDistance():
+        try:
+            value = BP.get_sensor(BP.PORT_1)
+            return value                            # print the distance in CM
+        except brickpi3.SensorError as error:
+            print(error)
 
     try:
         while(True):
-            if(getDistance() < 15):
+            print ("waiting for train to cross")
+            if(getDistance() < 12):
                 while(True):
-                    if(getDistance() > 30):
+                    print ("Train crossing")
+                    if(getDistance() > 40):
+                        print("train crossed")
+
+                        print("Motors should be running")
+                        time.sleep(.05)
                         BP.set_motor_power(BP.PORT_A + BP.PORT_D, speedAdvanceToGetOverTheRailsOfTheTrain)
+                        time.sleep(.05)
                         while(True):
-                            if(getDistance() < 3)
+                            print ("waiting to stop")
+                            if(getDistance() < 10):
                                 BP.set_motor_power(BP.PORT_A + BP.PORT_D, speedStop)
+                                return
                             time.sleep(0.02)
                     time.sleep(0.02)       
             time.sleep(0.02)    
 
     except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
         BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
-
-
-def: getDistance():
-    try:
-        value = BP.get_sensor(BP.PORT_1)
-        return value                            # print the distance in CM
-    except brickpi3.SensorError as error:
-        print(error)
-		
+            
+   
 main()
